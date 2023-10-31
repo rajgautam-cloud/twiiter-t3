e8b0f974a2458420d85eec08f2fc56c24559fbf9
rajgautam-material <105096247+rajgautam-material@users.noreply.github.com>
2023-10-31 12:52:26 +0530

    Added auth using clerk

---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export default authMiddleware({});
+
+export const config = {
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
