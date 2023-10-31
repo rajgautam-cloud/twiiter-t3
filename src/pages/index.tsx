36de30c7d1a1c8adbcc2a20a2756f26c336eb3a7
rajgautam-material <105096247+rajgautam-material@users.noreply.github.com>
2023-10-31 16:24:16 +0530

    Fetched post data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
+
   return (
     <>
       <Head>
@@ -20,6 +21,11 @@ export default function Home() {
           <div>
             {!user.isSignedIn && <SignInButton />}
             {!!user.isSignedIn && <SignOutButton />}
+          </div>
+          <div>
+            {data?.map((post) => (
+              <div key={post?.id}>{post?.content}</div>
+            ))}
           </div>
         </div>
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
